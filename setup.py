982ca75dffca45e4249ec4a6b20597663d9cc3fb
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-05-29 22:18:54 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if (sys.platform == "win32"):
+    base = "Win32GUI"    # Tells the build script to hide the console.
+
+includefiles = ['gui.ui']
+packages = ['queue']
+
+setup(
+	name = "CSGO VALUE CALCULATOR",
+	options = {'build_exe': {'include_files':includefiles, 'packages':packages}},
+	executables = [Executable("main.py", base=base)]
+)
