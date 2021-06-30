b88400dedc76d72fed85dbd5f7ce961060f2dab0
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-06-30 20:34:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ base = None
 if (sys.platform == "win32"):
     base = "Win32GUI"    # Tells the build script to hide the console.
 
-includefiles = ['gui.ui']
-packages = ['queue']
+includefiles = ["gui.ui"]
+packages = ['queue', 'webbrowser', 'tkinter', 'updater']
 
 setup(
 	name = "CSGO VALUE CALCULATOR",
-	version = "1.2"
+	version = "1.3",
+	author = "Mouad Essalim",
+	author_email="essalim99@gmail.com",
 	options = {'build_exe': {'include_files':includefiles, 'packages':packages}},
 	executables = [Executable("main.py", base=base)]
 )
