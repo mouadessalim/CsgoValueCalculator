41bb5170e14dbc9f0b95aa114603a4f5f4451b89
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-07-02 17:29:58 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -2,18 +2,21 @@ import requests
 from tkinter import *
 from tkinter import messagebox
 import webbrowser
+import json 
+import base64
 
 def updatefunc():
     try:
-        k = "1.3"
+        with open('config.json', 'r') as f:
+            data = json.load(f)
         response = requests.get("https://github.com/mouadessalim/CsgoValueCalculator/releases/latest")
-        if k == response.url[65:]:
+        if base64.b64decode(data["version_"].encode('ascii')).decode('ascii') == response.url[65:]:
             return 
         else:
             window = Tk()
             window.withdraw()
 
-            if messagebox.askyesno('Question', f' A new update is available, Download version {response.url[65:]} ?') == True:
+            if messagebox.askyesno('Update', f' A new update is available, Download version {response.url[65:]} ?') == True:
                 webbrowser.open("https://github.com/mouadessalim/CsgoValueCalculator/releases/latest")
             else:
                 return
