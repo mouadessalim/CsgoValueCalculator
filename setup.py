18076de67785ee6dd14f2b570a0467c5b6320488
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-07-08 23:25:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import sys
 from cx_Freeze import setup, Executable
+import base64
+import json
 
 base = None
 if (sys.platform == "win32"):
     base = "Win32GUI"    # Tells the build script to hide the console.
 
-includefiles = ["gui.ui", "config.json"]
-packages = ['queue', 'webbrowser', 'tkinter', 'updater', 'base64']
+includefiles = ["gui.ui", "config.json", "account.json"]
+packages = ['queue', 'tkinter', 'base64']
+
+with open('config.json', 'r') as f:
+    data = json.load(f)
 
 setup(
-	name = "CSGO VALUE CALCULATOR",
-	version = "1.3.1",
-	author = "Mouad Essalim",
-	author_email="essalim99@gmail.com",
+	name = base64.b64decode(data["name_"].encode('ascii')).decode('ascii'),
+	version = base64.b64decode(data["version_"].encode('ascii')).decode('ascii'),
+	author = base64.b64decode(data["author_"].encode('ascii')).decode('ascii'),
+	author_email=base64.b64decode(data["author_email_"].encode('ascii')).decode('ascii'),
 	options = {'build_exe': {'include_files':includefiles, 'packages':packages}},
 	executables = [Executable("main.py", base=base)]
 )
