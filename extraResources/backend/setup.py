e966d510d1190aa0ac6cf9d1f70700c8bc502549
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-09-04 22:55:09 +0100

    Update setup.py

--- extraResources/backend/setup.py
+++ extraResources/backend/setup.py
@@ -1,8 +1,9 @@
 from cx_Freeze import setup, Executable
 
 packages = ['queue']
+includefiles = ['chromedriver.exe']
 
 setup(
-	options = {'build_exe': {'packages':packages}},
+	options = {'build_exe': {'packages':packages, 'include_files':includefiles}},
 	executables = [Executable("csgovaluecalculator.py")]
 )
