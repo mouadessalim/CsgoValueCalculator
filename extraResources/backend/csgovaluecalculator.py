d611a7cc9c91bd76fbfbfd8ec7d6862c6404ed8a
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-09-05 20:44:01 +0100

    Update csgovaluecalculator.py

--- extraResources/backend/csgovaluecalculator.py
+++ extraResources/backend/csgovaluecalculator.py
@@ -14,8 +14,11 @@ def main_():
         if valid_response.ok:
             try:
                 chrome_params = Options()
-                chrome_params.headless = True
+                #chrome_params.headless = True
+                chrome_params.add_argument("--window-size=0,0")
+                chrome_params.add_argument("--log-level=3")
                 driver1 = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_params)
+                driver1.set_window_position(-10000,0)
                 driver1.get(f"https://csgobackpack.net//?nick={SteamID}")
                 element = WebDriverWait(driver1, 10).until(
                     EC.presence_of_element_located((By.XPATH, "/html/body//div[@id='info']//p"))
@@ -30,27 +33,7 @@ def main_():
                     conversion_finished = round(conversion, 2)
                     print(str(conversion_finished) + " " + responseip['currency'])
             except:
-                try:
-                    driver2 = webdriver.Chrome(executable_path='chromedriver.exe')
-                    driver2.set_window_position(-10000,0)
-                    driver2.set_window_size(0, 0)
-                    driver2.get(f"https://csgobackpack.net//?nick={SteamID}")
-                    element = WebDriverWait(driver2, 10).until(
-                        EC.presence_of_element_located((By.XPATH, "/html/body//div[@id='info']//p"))
-                    )
-                    data_float = float(element.text[:-1])
-                    responseip = requests.get("https://api.techniknews.net/ipgeo/").json()
-                    if responseip['currency'] == "EUR":
-                        print(str(data_float) + " EUR")
-                    else:
-                        response = requests.get(f"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/{responseip['currency'].lower()}.json").json()
-                        conversion = response[responseip['currency'].lower()] * data_float
-                        conversion_finished = round(conversion, 2)
-                        print(str(conversion_finished) + " " + responseip['currency'])
-                except:
-                    print("Server Down, please retry later !")
-                finally:
-                    driver2.quit()
+                print("Server Down, please retry later !")
             finally:
                 driver1.quit()
         else:
@@ -72,7 +55,6 @@ def get_version():
         driver3 = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_params)
         if 'browserVersion' in driver3.capabilities:
             v = driver3.capabilities['browserVersion']
-            
             if v[:2] == "93":
                 return True
             else:
