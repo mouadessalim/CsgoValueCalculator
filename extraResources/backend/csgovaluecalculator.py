ea5efe0902e8551e255b947e52b36b8e13e9d8fb
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-09-20 11:58:52 +0100

    Update csgovaluecalculator.py

--- extraResources/backend/csgovaluecalculator.py
+++ extraResources/backend/csgovaluecalculator.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from sys import argv
 import os
 import socket 
@@ -8,6 +9,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def writter(s, v):
+    if os.path.exists(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json"):
+        with open(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json", 'r') as f:
+            data = json.load(f)
+            data[s] = str(v)
+            with open(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json", 'w') as n:
+                n.write(str(json.dumps(data, indent=2)))
+    else:
+        with open(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json", 'w') as k:
+            k.write("{}")
+        with open(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json", 'r') as f:
+            data = json.load(f)
+            data[s] = str(v)
+            with open(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json", 'w') as n:
+                n.write(str(json.dumps(data, indent=2)))
 
 def main_():
     valid_response = requests.get(f"https://www.steamidfinder.com/lookup/{SteamID}")
@@ -25,17 +41,18 @@ def main_():
             data_float = float(element.text[:-1])
             responseip = requests.get("https://api.techniknews.net/ipgeo/").json()
             if responseip['currency'] == "EUR":
+                writter(SteamID, str(data_float))
                 print(str(data_float) + " EUR")
             else:
                 response = requests.get(f"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/{responseip['currency'].lower()}.json").json()
                 conversion = response[responseip['currency'].lower()] * data_float
                 conversion_finished = round(conversion, 2)
+                writter(SteamID, str(data_float))
                 print(str(conversion_finished) + " " + responseip['currency'])
         except:
             print("This inventory is private or user doesn't have any items in inventory")
         finally:
             driver1.quit()
-        
     else:
         print("This SteamID doesn't exist.")
 
