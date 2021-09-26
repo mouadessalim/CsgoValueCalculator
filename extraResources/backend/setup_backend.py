d2d52d3468a298534f5644abcb23318d7a920167
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-09-26 11:17:11 +0100

    Update setup_backend.py

--- extraResources/backend/setup_backend.py
+++ extraResources/backend/setup_backend.py
@@ -1,7 +1,8 @@
 from cx_Freeze import setup, Executable
+from csgovaluecalculator import version_chromedriver
 
 packages = ['queue']
-includefiles = ['chromedriver.exe']
+includefiles = [f'chromedriver_{version_chromedriver}.exe']
 
 setup(
 	options = {'build_exe': {'packages':packages, 'include_files':includefiles}},
