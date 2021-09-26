15828c7c511b1f4c18b0ec9e099d0680cfb760c5
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-09-26 11:16:17 +0100

    Update csgovaluecalculator.py

--- extraResources/backend/csgovaluecalculator.py
+++ extraResources/backend/csgovaluecalculator.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from sys import argv
+import sys
 import os
 import socket 
 from selenium import webdriver
@@ -8,6 +9,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+global version_chromedriver
+version_chromedriver = "94"
 
 def writter(s, v):
     if os.path.exists(f"{os.getenv('APPDATA')}\csgo-value-calculator\csgoaccount.json"):
@@ -32,7 +36,7 @@ def main_():
             chrome_params = Options()
             chrome_params.add_argument("--window-size=0,0")
             chrome_params.add_argument("--log-level=3")
-            driver1 = webdriver.Chrome(executable_path='PATH_TO_CHROMEDRIVER', chrome_options=chrome_params)
+            driver1 = webdriver.Chrome(executable_path='chromedriver_94.exe', chrome_options=chrome_params)
             driver1.set_window_position(-10000,0)
             driver1.get(f"https://csgobackpack.net//?nick={SteamID}")
             element = WebDriverWait(driver1, 10).until(
@@ -79,16 +83,16 @@ def get_version():
     try:
         chrome_params = Options()
         chrome_params.headless = True
-        driver3 = webdriver.Chrome(executable_path='PATH_TO_CHROMEDRIVER', chrome_options=chrome_params)
+        driver3 = webdriver.Chrome(executable_path='chromedriver_94.exe', chrome_options=chrome_params)
         if 'browserVersion' in driver3.capabilities:
             v = driver3.capabilities['browserVersion']
-            if v[:2] == "93":
+            if v[:2] == version_chromedriver:
                 return True
             else:
                 return False
         else:
             old_v = driver3.capabilities['version']
-            if old_v[:2] == "93":
+            if old_v[:2] == version_chromedriver:
                 return True
             else:
                 return False
@@ -100,32 +104,55 @@ def get_version():
         except:
             pass
 
-if test_connexion() and get_version() and check_server():
-    if argv[1].isdigit() and int(argv[1]) > 9999999999999999:
-        SteamID = argv[1]
-        main_()
-    else:
-        if not argv[1].isdigit():
-            chrome_params = Options()
-            chrome_params.headless = True
-            driver4 = webdriver.Chrome(executable_path='PATH_TO_CHROMEDRIVER', chrome_options=chrome_params)
-            driver4.get("https://steamid.io/")
-            try:
-                driver4.find_element(By.XPATH, "//input[@id='input']").send_keys(argv[1])
-                driver4.find_element(By.XPATH, "//button[@class='btn btn-danger input-lg']").click()
-                SteamID = driver4.find_element(By.XPATH, "//dd[@class='value short'][3]/a").text
-                main_()
-            except:
-                print('Steam profile link not found !')
+def verification():
+    if test_connexion() and get_version() and check_server():
+        if argv[1].isdigit() and int(argv[1]) > 9999999999999999:
+            global SteamID
+            SteamID = argv[1]
+            main_()
         else:
-            print("Format not valid!")
+            if not argv[1].isdigit():
+                chrome_params = Options()
+                chrome_params.headless = True
+                driver4 = webdriver.Chrome(executable_path='chromedriver_94.exe', chrome_options=chrome_params)
+                driver4.get("https://steamid.io/")
+                try:
+                    driver4.find_element(By.XPATH, "//input[@id='input']").send_keys(argv[1])
+                    driver4.find_element(By.XPATH, "//button[@class='btn btn-danger input-lg']").click()
+                    SteamID = driver4.find_element(By.XPATH, "//dd[@class='value short'][3]/a").text
+                    main_()
+                except:
+                    print('Steam profile link not found !')
+            else:
+                print("Format not valid!")
+    else:
+        if test_connexion() and check_server():
+            if os.path.exists('C:\Program Files\Google\Chrome\Application\chrome.exe') or os.path.exists('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'):
+                print(f'Chrome detected, but you need to have version {version_chromedriver}')
+            else:
+                print("Chrome not detected, please install it")
+        elif test_connexion():
+            print('Server down, please retry later.')
+        else:
+            print("No connexion found, please check it.")
+
+def declared():
+    try:
+        x = argv[1]
+        if x == "build":
+            return False
+        else:
+            return True
+    except:
+        return False
+
+if declared():
+    verification()
 else:
-    if test_connexion() and check_server():
-        if os.path.exists('C:\Program Files\Google\Chrome\Application\chrome.exe') or os.path.exists('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'):
-            print('Chrome detected, but you need to update it to version 93')
-        else:
-            print("Chrome not detected, please install it")
-    elif test_connexion():
-        print('Server down, please retry later.')
-    else:
-        print("No connexion found, please check it.")
+    try:
+        sys.exit()
+    except:
+        try:
+            quit()
+        except:
+            pass
