f7dfb4c33452238847166eaf5913bf565055171c
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2021-10-04 00:00:54 +0100

    Update csgovaluecalculator.py

--- csgovaluecalculator.py
+++ csgovaluecalculator.py
@@ -11,7 +11,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-global version_chromedriver
 version_chromedriver = "94"
 
 def writter(s, v):
