80725a809c5bb8e603889ba7601ccac74752d891
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2022-06-05 14:50:32 +0100

    Delete setup_uploader.py

--- setup_uploader.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-packages = ['queue']
-
-setup(
-    options = {'build_exe': {'packages':packages, 'include_msvcr': True}},
-	executables = [Executable("uploader_DS.py")]
-)
